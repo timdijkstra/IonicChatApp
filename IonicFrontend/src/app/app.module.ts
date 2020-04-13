370578141032477a1c925393a7168b33f0817246
Raymond <rjhho@avans.nl>
2020-04-13 15:13:47 +0200

    proxy goodies van martijn

--- IonicFrontend/src/app/app.module.ts
+++ IonicFrontend/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:8100/server', options: {} };
+//8100
+const martijnSpecial = '';
 
 @NgModule({
   declarations: [AppComponent],
